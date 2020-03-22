7ede77a00f865f5c650a8b6a5319e0a5f6fa2a29
adrskw <48623654+adrskw@users.noreply.github.com>
2020-03-22 16:15:34 +0100

    3. nawigacja z zakładkami

---
+++ AirMonitor/AirMonitor/Views/TabsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabsPage : Xamarin.Forms.TabbedPage
+    {
+        public TabsPage()
+        {
+            InitializeComponent();
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
